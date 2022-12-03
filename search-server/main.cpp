f64b3d95787eb5c30742cc5d3690f9216ee3a7fb
lvinnichenko <lvinnichenko@aligntech.com>
2022-12-03 22:48:33 +0100

    first commit to the project

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -2,3 +2,29 @@
 // Напишите ответ здесь:
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k = 0;
+
+    for (int i = 0; i <= 1000; i++)
+    {
+        const string str = to_string(i);
+        for (const char &digit : str)
+        {
+            if (digit == '3')
+            {
+                ++k;
+                break;
+            }
+        }
+    }
+
+    cout << k << endl; // 271 не вините строго, я был уставший и мне было так влом думать
+
+    return 0;
+}
