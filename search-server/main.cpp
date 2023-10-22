ab196041eccd262313a81644de0ff78094b0b803
lvinnichenko <lvinnichenko@aligntech.com>
2023-10-22 23:56:31 +0200

    brute force calculation

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -2,29 +2,27 @@
 // Напишите ответ здесь:
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
-#include <string>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
+int main() {
     int k = 0;
 
-    for (int i = 0; i <= 1000; i++)
-    {
+    for (int i = 0; i <= 1000; i++) {
         const string str = to_string(i);
-        for (const char &digit : str)
-        {
-            if (digit == '3')
-            {
+        for (const char &digit : str) {
+            if (digit == '3') {
                 ++k;
                 break;
             }
         }
     }
 
-    cout << k << endl; // 271 не вините строго, я был уставший и мне было так влом думать
+    cout << k << endl;  // 271 не вините строго, я был уставший и мне было так влом думать
+
+    // а еще это у меня повтор курса. репа старая (я думал заново пересоздается). я сделал hard reset force
 
     return 0;
 }
