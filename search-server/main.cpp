86d59d23dedf8f11f08c2441891323cca35b9557
lvinnichenko <lvinnichenko@aligntech.com>
2023-11-09 22:46:38 +0000

    adds reversed index and tf-idf relevance calculation

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,28 +1,194 @@
-// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
-
-// Закомитьте изменения и отправьте их в свой репозиторий.
+#include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <map>
+#include <set>
 #include <string>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    int k = 0;
+const int MAX_RESULT_DOCUMENT_COUNT = 5;
 
-    for (int i = 0; i <= 1000; i++) {
-        const string str = to_string(i);
-        for (const char &digit : str) {
-            if (digit == '3') {
-                ++k;
-                break;
+string ReadLine() {
+    string s;
+    getline(cin, s);
+    return s;
+}
+
+int ReadLineWithNumber() {
+    int result = 0;
+    cin >> result;
+    ReadLine();
+    return result;
+}
+
+vector<string> SplitIntoWords(const string& text) {
+    vector<string> words;
+    string word;
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
             }
+        } else {
+            word += c;
+        }
+    }
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+
+    return words;
+}
+
+struct Document {
+    int id;
+    double relevance;
+};
+
+class SearchServer {
+   public:
+    void SetStopWords(const string& text) {
+        for (const string& word : SplitIntoWords(text)) {
+            stop_words_.insert(word);
         }
     }
 
-    cout << k << endl;  // 271 не вините строго, я был уставший и мне было так влом думать
+    void AddDocument(int document_id, const string& document) {
+        const vector<string> words = SplitIntoWordsNoStop(document);
 
-    // а еще это у меня повтор курса. репа старая (я думал заново пересоздается). я сделал hard reset force
+        const double inv_count = 1.0 / words.size();
 
-    return 0;
+        for (const string& word : words) {
+            word_to_document_freqs_[word][document_id] += inv_count;
+        }
+
+        document_count_ += 1;
+    }
+
+    vector<Document> FindTopDocuments(const string& raw_query) const {
+        const Query query = ParseQuery(raw_query);
+        auto matched_documents = FindAllDocuments(query);
+
+        sort(matched_documents.begin(), matched_documents.end(),
+             [](const Document& lhs, const Document& rhs) {
+                 return lhs.relevance > rhs.relevance;
+             });
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+        }
+        return matched_documents;
+    }
+
+   private:
+    struct QueryWord {
+        string data;
+        bool is_minus;
+    };
+
+    struct Query {
+        set<string> plus_words;
+        set<string> minus_words;
+    };
+
+    map<string, map<int, double>> word_to_document_freqs_;
+    int document_count_ = 0;
+
+    set<string> stop_words_;
+
+    double CalculateIDF(const string& word) const {
+        return log(static_cast<double>(document_count_) / static_cast<double>(word_to_document_freqs_.at(word).size()));
+    }
+
+    QueryWord ParseQueryWord(string text) const {
+        bool is_minus = false;
+        if (text[0] == '-') {
+            is_minus = true;
+            text = text.substr(1);
+        }
+        return {text, is_minus};
+    }
+
+    Query ParseQuery(const string& text) const {
+        SearchServer::Query query;
+        for (string word : SplitIntoWordsNoStop(text)) {
+            SearchServer::QueryWord query_word = ParseQueryWord(word);
+
+            if (query_word.is_minus) {
+                query.minus_words.insert(query_word.data);
+            } else {
+                query.plus_words.insert(query_word.data);
+            }
+        }
+        return query;
+    }
+
+    bool IsStopWord(const string& word) const {
+        return stop_words_.count(word) > 0;
+    }
+
+    vector<string> SplitIntoWordsNoStop(const string& text) const {
+        vector<string> words;
+        for (const string& word : SplitIntoWords(text)) {
+            if (!IsStopWord(word)) {
+                words.push_back(word);
+            }
+        }
+        return words;
+    }
+
+    vector<Document> FindAllDocuments(const Query& query) const {
+        map<int, double> document_to_relevance;
+
+        for (const string& word : query.plus_words) {
+            if (word_to_document_freqs_.count(word) > 0) {
+                double idf = CalculateIDF(word);
+
+                for (const auto [id, tf] : word_to_document_freqs_.at(word)) {
+                    document_to_relevance[id] += tf * idf;
+                }
+            }
+        }
+
+        for (const string& word : query.minus_words) {
+            if (word_to_document_freqs_.count(word) > 0) {
+                for (const auto [id, tf] : word_to_document_freqs_.at(word)) {
+                    document_to_relevance.erase(id);
+                }
+            }
+        }
+
+        vector<Document> matched_documents;
+
+        for (const auto& [id, relevance] : document_to_relevance) {
+            matched_documents.push_back({id, relevance});
+        }
+
+        return matched_documents;
+    }
+};
+
+SearchServer CreateSearchServer() {
+    SearchServer search_server;
+    search_server.SetStopWords(ReadLine());
+
+    const int document_count = ReadLineWithNumber();
+    for (int document_id = 0; document_id < document_count; ++document_id) {
+        search_server.AddDocument(document_id, ReadLine());
+    }
+
+    return search_server;
+}
+
+int main() {
+    const SearchServer search_server = CreateSearchServer();
+
+    const string query = ReadLine();
+    for (const auto& [document_id, relevance] : search_server.FindTopDocuments(query)) {
+        cout << "{ document_id = "s << document_id << ", "
+             << "relevance = "s << relevance << " }"s << endl;
+    }
 }
